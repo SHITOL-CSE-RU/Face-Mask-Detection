2db52a855cedc8db2190e70bf6888fe57feba037
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-09-01 22:55:43 +0600

    Final Push

--- test.py
+++ test.py
@@ -18,7 +18,47 @@ def preprocessing(img):
     img = img/255
     return img
 
+def get_className(classNo):
+	if classNo==0:
+		return "Mask"
+	elif classNo==1:
+		return "No Mask"
 
+
+while True:
+	sucess, imgOrignal=cap.read()
+	faces = facedetect.detectMultiScale(imgOrignal,1.3,5)
+	for x,y,w,h in faces:
+		# cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(50,50,255),2)
+		# cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (50,50,255),-2)
+		crop_img=imgOrignal[y:y+h,x:x+h]
+		img=cv2.resize(crop_img, (32,32))
+		img=preprocessing(img)
+		img=img.reshape(1, 32, 32, 1)
+		# cv2.putText(imgOrignal, "Class" , (20,35), font, 0.75, (0,0,255),2, cv2.LINE_AA)
+		# cv2.putText(imgOrignal, "Probability" , (20,75), font, 0.75, (255,0,255),2, cv2.LINE_AA)
+		prediction=model.predict(img)
+		classIndex=model.predict_classes(img)
+		probabilityValue=np.amax(prediction)
+		if probabilityValue>threshold:
+			if classIndex==0:
+				cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
+				cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
+				cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
+			elif classIndex==1:
+				cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(50,50,255),2)
+				cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (50,50,255),-2)
+				cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
+
+			# cv2.putText(imgOrignal,str(round(probabilityValue*100, 2))+"%" ,(180, 75), font, 0.75, (255,0,0),2, cv2.LINE_AA)
+	cv2.imshow("Result",imgOrignal)
+	k=cv2.waitKey(1)
+	if k==ord('q'):
+		break
+
+
+cap.release()
+cv2.destroyAllWindows()
 
 
 
